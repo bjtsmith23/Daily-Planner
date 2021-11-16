1de46c600cd2948eed896cb35e5ef64bc433bc79
Brian Smith <bjtsmith23@gmail.com>
2021-11-16 16:21:20 -0500

    updated time with colors added

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,22 @@
-var timeDisplayEl = $('#time-display');
+var timeDisplayEl = $('#currentDay');
 
 
+console.log(moment().hour());
+var currentHour = moment().hour();
+var textareaEl = $('textarea');
+console.log(currentHour);
+// console.log(textareaEl);
 
+for (var i = 0; i < textareaEl.length; i++) {
+  console.log(textareaEl[i].id);
+  if (textareaEl[i].id < currentHour) {
+    textareaEl[i].classList.add('past');
+} else if (textareaEl[i].id == currentHour) {
+    textareaEl[i].classList.add('present');
+  } else {
+    textareaEl[i].classList.add('future')
+  }
+}
 
 function displayTime() {
     var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
