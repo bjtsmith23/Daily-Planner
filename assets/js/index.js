1be9c931da4c4e38be9f4fc6259464f24c86be1a
Brian Smith <bjtsmith23@gmail.com>
2021-11-23 17:21:46 -0500

    added javascript to save text to local storage

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,8 @@
 var timeDisplayEl = $('#currentDay');
+var allButtons = document.getElementsByTagName('button');
+for ( var i = 0; i < allButtons.length; i++) {
+  allButtons[i].addEventListener('click', saveText)
+}
 
 
 console.log(moment().hour());
@@ -26,15 +30,38 @@ function displayTime() {
   setInterval(displayTime, 1000);
 
   function saveText(){
-    var title = document.getElementById("title_box").value;
-    var text = document.getElementById("text_box").value;
+    console.log(this.previousElementSibling.value);
+    console.log(this.previousElementSibling.id);
     
-    document.getElementById("title_space").innerHTML = title;
-    document.getElementById("text_space").innerHTML = text;
+    localStorage.setItem(this.previousElementSibling.id, this.previousElementSibling.value);
+
+
+
+
+
+    // var title = document.getElementById("title_box").value;
+    // var text = document.getElementById("text_box").value;
     
-  }
+    // document.getElementById("title_space").innerHTML = title;
+    // document.getElementById("text_space").innerHTML = text;
+    
+  
   function br(){
     var actualtext = document.getElementById("text_box").value;
     var processedtext = actualtext + "<br />";
     document.getElementById("text_box").innerHTML = processedtext;
   }
+  }
+  // localStorage.setItem("Hi", "Hello");
+  // var getHello = localStorage.getItem("Hi");
+  // console.log(getHello);
+
+
+
+  for (var i = 9; i < 18; i++) {
+    var localStorageValue = localStorage.getItem(i);
+    console.log(localStorageValue);
+    document.getElementById(i).value = localStorageValue;
+    console.log(i);
+  }
+ 
